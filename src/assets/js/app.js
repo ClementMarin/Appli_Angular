980328e2c5d848467b653ecd686a41f80fe63d0d
Marin Clément <cle.marin.38@gmail.com>
2015-11-09 14:22:06 +0100

    Ajout ngRoute (module Angular) + paramètrage

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,19 @@
 (function(angular){
   'use strict';
 
-  angular.module('myApp',['helloModule', 'contactModule', 'checkinModule']);
+  angular.module('myApp',[
+    'ngRoute',
+    'helloModule', 'contactModule', 'checkinModule'
+  ])
+
+  .config(function($routeProvider){
+      $routeProvider
+        .when('/', {
+          templateUrl: 'assets/template/checkinList.html',
+        })
+        .when('/checkin/:checkinId',{
+          templateUrl: 'assets/template/checkinDetails.html',
+        });
+  });
 
 })(window.angular);
